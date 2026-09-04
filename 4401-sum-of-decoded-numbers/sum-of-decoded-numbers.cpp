574e4a64f5c778ce585d83714b5ce201015c1087
Rajat4007 <214415765+Rajat4007@users.noreply.github.com>
2026-09-04 12:07:09 +0530

    Time: 114 ms (52.85%) | Memory: 91.1 MB (23.45%) - LeetSync

---
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long modPow(long long base, long long exp, long long mod) {
+        long long res = 1;
+        base %= mod;
+        while (exp > 0) {
+            if (exp & 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return res;
+    }
+    int sumDecoded(vector<long long>& nums) {
+        int n = nums.size();
+        long long sum_decoded = 0;
+        const int mod = 1e9 +7;
+        for(int i= 0; i<n; i++){
+            long long num = nums[i];
+            int width = num%10;
+            long long d = num/10;
+            string d_str = to_string(d);
+            long long x = stoll(d_str.substr(0,width));
+            long long y = stoll(d_str.substr(width));
+            
+            sum_decoded = (sum_decoded + modPow(x,y,mod))%mod;
+        }
+        return sum_decoded;
+    }
+};
+
+/*Floating-Point & Integer Overflow: std::pow(x, y) returns a double and quickly overflows 64-bit integers when $y$ is large (up to $10^9$). You need modular binary exponentiation ((base^exp) % mod).*/
