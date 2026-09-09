26475847b62e299be0757aecb3e892452ed9772c
Rajat4007 <214415765+Rajat4007@users.noreply.github.com>
2026-09-09 07:52:21 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (84.80%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if (n < 1000) {
-            return 0;
-        } else if (n < 1000000) {
-            return n - 999;
-        } else if (n < 1000000000) {
-            return 1LL * 999000 + (n - 999999) * 2;
-        } else if (n < 1000000000000LL) {
-            return 1LL * 999000 + 2LL * 999000000 + (n - 999999999) * 3;
-        } else if (n < 1000000000000000LL) {
-            return 1LL * 999000 + 2LL * 999000000 + 3LL * 999000000000LL + (n - 999999999999LL) * 4;
-        } else {
-            return 1LL * 999000 + 2LL * 999000000 + 3LL * 999000000000LL + 4LL * 999000000000000LL + (n - 999999999999999LL) * 5;
+        long long total = 0;
+        for (long long base = 1000; base <= n; base *= 1000) {
+            total += (n - base + 1);
+            
         }
+        return total;
     }
 };
